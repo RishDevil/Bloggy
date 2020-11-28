989b98684018bd04d08698111d0273337861fab1
RishDevil <rishabhchaubey13@gmail.com>
2020-11-28 09:10:49 +0530

    make it better

--- bloggy/src/webpack.config.js
+++
@@ -1,9 +0,0 @@
-const { dirname } = require("path");
-const path = require("path");
-
-module.exports = {
-  devServer: {
-    historyApiFallBack: true,
-  },
-  entry: path.resolve(__dirname, "./src/index.js"),
-};
