d10ce6fd9d529c12169bd574f241675b1acfb381
RishDevil <rishabhchaubey13@gmail.com>
2020-11-28 09:39:59 +0530

    make it better

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ if (process.env.NODE_ENV === "production") {
   console.log("production");
   app.use(express.static("bloggy/build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "bloggy", "build", "index.html"));
+    res.sendFile(path.join(__dirname, "bloggy/build/index.html"));
   });
 }
 
